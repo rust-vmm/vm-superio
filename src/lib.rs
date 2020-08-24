7c34147ee419c7996e30463086fb8532ef268920
Laura Loghin <lauralg@amazon.com>
2020-08-24 11:51:00 +0300

    initial serial console implementation
    Signed-off-by: Laura Loghin <lauralg@amazon.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Portions Copyright 2017 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the THIRD-PARTY file.
+//
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+//! Emulation for legacy devices.
+//!
+//! For now, it offers emulation support only for the Linux serial console.
+
 #![deny(missing_docs)]
-//! Dummy crate needs high-level documentation.
-/// Dummy public function needs documentation.
-pub fn it_works() {
-    assert!(true);
-}
+
+pub mod serial;
+
+pub use serial::Serial;
