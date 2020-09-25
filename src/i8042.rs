e5fe610b584e6373d3dda46914106d28a021df7c
Laura Loghin <lauralg@amazon.com>
2020-09-25 18:54:03 +0300

    i8042: add documentation example
    Signed-off-by: Laura Loghin <lauralg@amazon.com>

--- src/i8042.rs
+++ src/i8042.rs
@@ -29,6 +29,43 @@ pub type Result<E> = result::Result<(), E>;
 ///
 /// A [`Trigger`](../trait.Trigger.html) object is used for notifying the VMM
 /// about the CPU reset event.
+///
+/// # Example
+///
+/// ```rust
+/// # use std::io::{Error, Result};
+/// # use std::ops::Deref;
+/// # use vm_superio::Trigger;
+/// # use vm_superio::I8042Device;
+/// # use vmm_sys_util::eventfd::EventFd;
+///
+/// struct EventFdTrigger(EventFd);
+/// impl Trigger for EventFdTrigger {
+///     type E = Error;
+///
+///     fn trigger(&self) -> Result<()> {
+///         self.write(1)
+///     }
+/// }
+/// impl Deref for EventFdTrigger {
+///     type Target = EventFd;
+///     fn deref(&self) -> &Self::Target {
+///         &self.0
+///     }
+/// }
+/// impl EventFdTrigger {
+///     pub fn new(flag: i32) -> Self {
+///         EventFdTrigger(EventFd::new(flag).unwrap())
+///     }
+/// }
+///
+/// let reset_evt = EventFdTrigger::new(libc::EFD_NONBLOCK);
+/// let mut i8042 = I8042Device::new(reset_evt);
+///
+/// // Check read/write operations.
+/// assert_eq!(i8042.read(0), 0);
+/// i8042.write(4, 0xFE).unwrap();
+/// ```
 pub struct I8042Device<T: Trigger> {
     /// CPU reset event object. We will trigger this event when the guest issues
     /// the reset CPU command.
@@ -38,6 +75,15 @@ pub struct I8042Device<T: Trigger> {
 impl<T: Trigger> I8042Device<T> {
     /// Constructs an i8042 device that will signal the given event when the
     /// guest requests it.
+    ///
+    /// # Arguments
+    /// * `reset_evt` - A Trigger object that will be used to notify the driver
+    ///                 about the reset event.
+    ///
+    /// # Example
+    ///
+    /// You can see an example of how to use this function in the
+    /// [`Example` section from `I8042Device`](struct.I8042Device.html#example).
     pub fn new(reset_evt: T) -> I8042Device<T> {
         I8042Device { reset_evt }
     }
@@ -51,6 +97,11 @@ impl<T: Trigger> I8042Device<T> {
     /// # Arguments
     /// * `_offset` - The offset that will be added to the base address
     ///              for writing to a specific register.
+    ///
+    /// # Example
+    ///
+    /// You can see an example of how to use this function in the
+    /// [`Example` section from `I8042Device`](struct.I8042Device.html#example).
     pub fn read(&mut self, _offset: u8) -> u8 {
         0x00
     }
@@ -62,6 +113,11 @@ impl<T: Trigger> I8042Device<T> {
     /// * `offset` - The offset that will be added to the base address
     ///              for writing to a specific register.
     /// * `value` - The byte that should be written.
+    ///
+    /// # Example
+    ///
+    /// You can see an example of how to use this function in the
+    /// [`Example` section from `I8042Device`](struct.I8042Device.html#example).
     pub fn write(&mut self, offset: u8, value: u8) -> Result<T::E> {
         match offset {
             COMMAND_OFFSET if value == CMD_RESET_CPU => {
