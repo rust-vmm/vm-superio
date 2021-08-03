b7cd601861e8128e6a97d3ddd966a3d89eafdf89
Laura Loghin <lauralg@amazon.com>
2021-08-03 10:45:58 +0300

    create vm-superio-ser
    This crate provides state objects that add serialization
    and versionize capabilities to the ones from vm-superio.
    
    Signed-off-by: Laura Loghin <lauralg@amazon.com>

---
+++ crates/vm-superio-ser/src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+//! Adds to the state objects from `vm-superio` serialization capabilities.
+//!
+//! Provides wrappers over the state objects from `vm-superio` crate which
+//! implement the `Serialize`, `Deserialize` and `Versionize` traits as well.
+
+#![deny(missing_docs)]
